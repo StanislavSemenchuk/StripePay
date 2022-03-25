f9e51d769f2a7c83156d695c2a1391745c7546d6
Stanislav Semenchuk <staniswlavsemenchuk@gmail.com>
2022-03-25 11:02:40 +0200

    First steps, added scratch of webhook and billing services

---
+++ StripePay/Services/Billing/BillingService.cs
@@ -0,0 +1,24 @@
+﻿using Stripe;
+using Stripe.Checkout;
+
+namespace StripePay.Services.Billing;
+
+public interface IBillingService
+{
+}
+
+public class BillingService : IBillingService
+{
+    private readonly PayoutService _payoutService;
+    private readonly SessionService _sessionService;
+    public BillingService(PayoutService payoutService, SessionService sessionService)
+    {
+        _payoutService = payoutService;
+        _sessionService = sessionService;
+    }
+
+    public async Task CheckoutAsync()
+    {
+
+    }
+}
---
+++ StripePay/Services/WebHook/WebHookService.cs
@@ -0,0 +1,47 @@
+﻿using Stripe;
+using DotNetEnv;
+
+namespace StripePay.Services.WebHook;
+
+public interface IWebHookService
+{
+    public Task<WebhookEndpoint> CreateWebHookAsync(WebhookEndpointCreateOptions options);
+}
+
+public class WebHookService : IWebHookService
+{
+    private readonly WebhookEndpointService _endpointService;
+    public WebHookService(WebhookEndpointService endpointService)
+    {
+        _endpointService = endpointService;
+    }
+    public async Task<WebhookEndpoint> CreateWebHookAsync(WebhookEndpointCreateOptions options)
+    {
+        var webhookEndpoint = new WebhookEndpoint();
+        if (await TryCreateWebHookAsync(options, webhookEndpoint))
+        {
+            return webhookEndpoint;
+        }
+        else 
+        {
+            throw new InvalidOperationException(webhookEndpoint.StripeResponse.Content);
+        }
+    }
+    private async Task<bool> TryCreateWebHookAsync(WebhookEndpointCreateOptions options, WebhookEndpoint webhookEndpoint)
+    {
+        try
+        {
+            webhookEndpoint = await _endpointService.CreateAsync(options);
+            return true;
+        }
+        catch (StripeException ex)
+        {
+            webhookEndpoint = new WebhookEndpoint()
+            {
+                StripeResponse = ex.StripeResponse
+            };
+            return false;
+        }
+
+    }
+}
